93c696887a28e59cd25dca1de939761cc0dd8d67
irtripathi <irtripathi@falabella.cl>
2022-06-30 11:08:11 +0530

    sample class added new changes

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class SampleClass {
+public class SampleClass {
+    public static void main(String[] args) {
+        System.out.println("Sample class");
+        System.out.println("Sample class sout");
+    }
 }
